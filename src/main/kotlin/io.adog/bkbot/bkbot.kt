eb07805d215bbea640d590da437faeb22a387753
WMXPY <wm@wmpcxpy.com>
2017-09-14 22:02:18 -0500

    update examples

--- src/main/kotlin/io.adog/bkbot/bkbot.kt
+++ src/main/kotlin/io.adog/bkbot/bkbot.kt
@@ -8,6 +8,8 @@ class bkbot(){
     }
 
     public fun run(){
-        console.log(this.sep.a("翁爷的鸡至少七万5千四百二十八厘米"));
+        console.log(this.sep.a("翁爷的鸡巴至少七万5千四百二十八厘米"));
+        console.log(this.sep.a("我在车里喝水"));
+        console.log(this.sep.a("Nana今天吃了很多屎"));
     }
 }
