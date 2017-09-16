d26bd81002018f0d48938fd19da11d06490ad1ed
WMXPY <wm@wmpcxpy.com>
2017-09-16 16:05:41 -0500

    mac tested fine

--- src/bin/bash.ts
+++ src/bin/bash.ts
@@ -9,17 +9,18 @@ const bash = new bashHelper();
 let parseList = process.argv;
 parseList.splice(0, 2);
 
-// bkbot.io.adog.bkbot.main_kand9s$(parseList);
-interval();
+// bkbot.io.adog.bkbot.main_kand9s$(parseList);'
+// bash.setClip("test");
+// interval();
 
-let clipboard = "";
-function interval() {
-    bash.getClip(function (out: string) {
-        let a = out;
-        if (a != clipboard) {
-            clipboard = a;
-            console.log(clipboard);
-        }
-        setTimeout(interval, 500);
-    })
-}
+// let clipboard = "";
+// function interval() {
+//     bash.getClip(function (out: string) {
+//         let a = out;
+//         if (a != clipboard) {
+//             clipboard = a;
+//             console.log(clipboard);
+//         }
+//         setTimeout(interval, 500);
+//     })
+// }
